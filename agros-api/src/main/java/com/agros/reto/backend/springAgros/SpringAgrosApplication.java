ead5258ec78040bb6ee60deabc90bd6ba404039e
Joel Quispe <joel.aqs7@gmail.com>
2023-02-12 15:53:20 -0500

    Reto Agros backend v6

--- agros-api/src/main/java/com/agros/reto/backend/springAgros/SpringAgrosApplication.java
+++ agros-api/src/main/java/com/agros/reto/backend/springAgros/SpringAgrosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringtennisApplication {
+public class SpringAgrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringtennisApplication.class, args);
+		SpringApplication.run(SpringAgrosApplication.class, args);
 	}
 
 }
